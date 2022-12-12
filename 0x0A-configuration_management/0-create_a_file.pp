8819d408683b758085dce0b9fd6d3e4adb6b9f85
Mdigo12 <mdigojacob@gmail.com>
2022-12-12 00:57:28 -0800

    comment rmoved double quotes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# create a file in /tmp with specific requirements
+
 file { 'school': # resource type file and filename
   path    => '/tmp/school',
   mode    => '0744',   # file permissions
   owner   => 'www-data',
   group   => 'www-data',
-  content => "I love Puppet", # file content
+  content => 'I love Puppet', # file content
 }
