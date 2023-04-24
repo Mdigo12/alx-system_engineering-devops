fe457e29a0cfa513b00f8ac5afdb434aa64412e3
Mdigo12 <mdigojacob@gmail.com>
2023-04-24 23:21:57 +0300

    json with all users and their tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,17 +4,17 @@ Using `https://jsonplaceholder.typicode.com/` REST API
 Use urllib or requests module
 
 Script to export data in the CSV format
-    Records all tasks that are owned by this employee
-    Format must be: { "USER_ID": [{"task": "TASK_TITLE",\
-        "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"},\
-            {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,\
-                  "username": "USERNAME"}, ... ]}
+    Records all tasks that are owned by each employee
+    Format must be: { "USER_ID": [{"username": "USERNAME",\
+        "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS},\
+            {"username": "USERNAME", "task": "TASK_TITLE",\
+                 "completed": TASK_COMPLETED_STATUS,\
+                  }, ... ]}
 
-File name must be: USER_ID.json
+File name must be: todo_all_employees.json
 """
 import json
 import requests
-from sys import argv
 
 if __name__ == '__main__':
 
@@ -24,20 +24,16 @@ if __name__ == '__main__':
     To a user `https://jsonplaceholder.typicode.com/users/2/`
     To a user's todos `https://jsonplaceholder.typicode.com/users/2/todos`
     """
-    # user_id = argv[1]
     base_url = 'https://jsonplaceholder.typicode.com/'
     users_url = base_url + 'users/'
-    # user_todos_url = users_url + user_id + '/todos'
-    # user = requests.get(users_url + user_id).json()
-    # user_todos = requests.get(user_todos_url).json()
     file_name = 'todo_all_employees.json'
 
     with open(file_name, 'w') as f:
         users_dict = {}
         for user in requests.get(users_url).json():
-            user_id = user.get('id')
+            user_id = str(user.get('id'))
             tasks_list = []
-            user_todos_url = users_url + str(user_id) + '/todos'
+            user_todos_url = users_url + user_id + '/todos'
             user_todos = requests.get(user_todos_url).json()
             for todo in user_todos:
                 task_dict = {"username": user.get('username'),
@@ -46,5 +42,4 @@ if __name__ == '__main__':
                 tasks_list.append(task_dict)
             user_dict = {user_id: tasks_list}
             users_dict.update(user_dict)
-        users_dict.update(user_dict)
         json.dump(users_dict, f)
